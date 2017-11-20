90e6bb6ea6f575ac9492cfa9cdf175ff6345ca77
Marc Magon <ged_marc@hotmail.com>
2017-11-20 20:46:06 +0200

    Test Cases

---
+++ test/za/co/mmagon/jwebswing/PlaceHolderTest.java
@@ -0,0 +1,14 @@
+package za.co.mmagon.jwebswing;
+
+import com.armineasy.injection.GuiceContext;
+import org.junit.jupiter.api.Test;
+
+class PlaceHolderTest extends BaseTestClass
+{
+
+	@Test
+	public void testGuice()
+	{
+		GuiceContext.inject();
+	}
+}
