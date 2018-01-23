caacfe67b5744888e902c0bbf99bf2c7f591c5c1
Marc Magon <ged_marc@hotmail.com>
2018-01-23 20:19:52 +0200

    Hmmm

--- src/za/co/mmagon/jwebswing/plugins/jquery/JQueryReferencePool.java
+++ src/za/co/mmagon/jwebswing/plugins/jquery/JQueryReferencePool.java
@@ -29,7 +29,7 @@ public enum JQueryReferencePool
 	JQuery(new JQueryReference()),
 	JQueryV2(new JavascriptReference("JQuery", 2.24, "bower_components/jquery/dist/jquery.js", "https://code.jquery.com/jquery-2.2.4.js").setPriority(RequirementsPriority.First)),
 
-	JQueryV3(new JavascriptReference("Jquery3", 3.1, "bower_components/jquery-3/dist/core.min.js", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", 0, RequirementsPriority.First).setCordovaRequired(true)),
+	JQueryV3(new JavascriptReference("Jquery3", 3.1, "bower_components/jquery-3/dist/jquery.min.js", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", 0, RequirementsPriority.First).setCordovaRequired(true)),
 
 	JQueryMigrate(new JQueryMigrateReference()),
 
