e58f4d8bd3bcc4f88f3908632420e7f538b1d779
GedMarc <ged_marc@hotmail.com>
2018-12-25 21:01:24 +0200

    Add data source injects for c3p0 connections

--- src/main/java/com/jwebmp/core/base/vue/VueFeature.java
+++ src/main/java/com/jwebmp/core/base/vue/VueFeature.java
@@ -8,11 +8,17 @@ import javax.validation.constraints.NotNull;
 
 /**
  * This feature adds Vue as the preferred provider
+ *
  * @param <J>
  */
 public class VueFeature<J extends VueFeature<J>>
-		extends Feature<VueFeature, JavaScriptPart,J>
+		extends Feature<VueFeature, JavaScriptPart, J>
 {
+
+	/**
+	 * The name of this vue application
+	 */
+	private static String appName = "appDomId";
 
 	public VueFeature(ComponentHierarchyBase component)
 	{
@@ -21,12 +27,36 @@ public class VueFeature<J extends VueFeature<J>>
 
 	}
 
+	/**
+	 * Method getAppName returns the appName of this VueFeature object.
+	 * <p>
+	 * The name of this vue application
+	 *
+	 * @return the appName (type String) of this VueFeature object.
+	 */
+	public static String getAppName()
+	{
+		return appName;
+	}
+
+	/**
+	 * Method setAppName sets the appName of this VueFeature object.
+	 * <p>
+	 * The name of this vue application
+	 *
+	 * @param appName
+	 * 		the appName of this VueFeature object.
+	 */
+	public static void setAppName(String appName)
+	{
+		VueFeature.appName = appName;
+	}
+
 	@Override
 	protected void assignFunctionsToComponent()
 	{
-
+		
 	}
-
 
 	/**
 	 * Renders the JavaScript for this Builder
@@ -41,6 +71,4 @@ public class VueFeature<J extends VueFeature<J>>
 	{
 		return new StringBuilder();
 	}
-
-
 }
