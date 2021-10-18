1d718160cf51a6f85434e8b175a6a37204c0e9f8
GedMarc <marc.magon@gmail.com>
2021-10-18 23:29:08 +0200

    JWebMP multipage updates and fixes for some bootstrap events

--- src/main/java/com/jwebmp/core/base/ajax/JsonVariable.java
+++ src/main/java/com/jwebmp/core/base/ajax/JsonVariable.java
@@ -16,24 +16,20 @@
  */
 package com.jwebmp.core.base.ajax;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.guicedee.guicedinjection.interfaces.ObjectBinderKeys;
-import com.guicedee.guicedinjection.representations.IJsonRepresentation;
-import com.jwebmp.core.htmlbuilder.javascript.JavaScriptPart;
-import com.guicedee.guicedinjection.GuiceContext;
-import com.guicedee.logger.LogFactory;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.type.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.*;
+import com.guicedee.guicedinjection.*;
+import com.guicedee.guicedinjection.interfaces.*;
+import com.guicedee.guicedinjection.representations.*;
+import com.guicedee.logger.*;
+import com.jwebmp.core.htmlbuilder.javascript.*;
 
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 /**
  * DTO for angular variable transfers forward and back
@@ -43,242 +39,282 @@ import java.util.logging.Logger;
  */
 @SuppressWarnings({"JavaDoc", "unused"})
 public class JsonVariable
-        extends JavaScriptPart<JsonVariable> {
-    private static final Logger log = LogFactory.getLog("JsonVariable");
-
-    /**
-     * The variable name to use
-     */
-    private String variableName;
-    /**
-     * The object to assign
-     */
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    @JsonDeserialize(using = ObjectToStringDeserialize.class)
-    @JsonSerialize(using = AlwaysEmptySerializer.class)
-    private String variableText;
-    /**
-     * The actual variable object
-     */
-    private Object variable;
-    /**
-     * The owner id of this DTO
-     */
-    @JsonProperty(value = "$jwid")
-    private String ownerId;
-
-    /**
-     * Constructs a blank object
-     */
-    public JsonVariable() {
-    }
-
-    /**
-     * Constructs a new instance with a given name and object
-     *
-     * @param variableName   The variable name to apply
-     * @param variableObject The JSON object to use
-     */
-    public JsonVariable(String variableName, String variableObject) {
-        this.variableName = variableName;
-        variableText = variableObject;
-        variable = variableText;
-    }
-
-    /**
-     * Constructs a new instance with a given name and object
-     *
-     * @param variableName   The variable name to apply
-     * @param variableObject The JSON object to use
-     */
-    public JsonVariable(String variableName, Object variableObject) {
-        this.variableName = variableName;
-        variable = variableObject;
-    }
-
-    /**
-     * Constructs a new instance with a given name and object
-     *
-     * @param variableName   The variable name to apply
-     * @param variableObject The JSON object to use
-     */
-    public JsonVariable(String variableName, JavaScriptPart<?> variableObject) {
-        this.variableName = variableName;
-        variable = variableObject;
-    }
-
-    /**
-     * Gets the DTO Form of this object
-     *
-     * @param <T>       The type this object actually is
-     * @param classType The class type to return
-     * @return The DTO direct from the call
-     * @throws IOException
-     */
-    public <T extends Serializable> T getDto(Class<T> classType) throws IOException {
-        T output = IJsonRepresentation.From(variableText, classType);
-        variable = output;
-        return output;
-    }
-
-    /**
-     * Gets the variable name
-     *
-     * @return
-     */
-    public String getVariableName() {
-        return variableName;
-    }
-
-    /**
-     * Sets the variable name
-     *
-     * @param variableName
-     */
-    public void setVariableName(String variableName) {
-        this.variableName = variableName;
-    }
-
-    /**
-     * Returns the variable if any is applied
-     *
-     * @return
-     */
-    public Object getVariable() {
-        return variable;
-    }
-
-    /**
-     * Sets the variable if any is applied
-     *
-     * @param variable
-     */
-    public void setVariable(Serializable variable) {
-        this.variable = variable;
-    }
-
-    /**
-     * Sets the owning component ID
-     *
-     * @return
-     */
-    @Override
-    public String getReferenceId() {
-        return ownerId;
-    }
-
-    /**
-     * Gets the owning component ID
-     *
-     * @param referenceId
-     */
-    @Override
-    public void setReferenceId(String referenceId) {
-        ownerId = referenceId;
-    }
-
-    /**
-     * @return
-     */
-    @SuppressWarnings("Convert2Diamond")
-    public Map<String, Object> asMap() {
-        String variableText = getVariableText();
-        TypeReference<HashMap<String, Object>> typeRef
-                = new TypeReference<HashMap<String, Object>>() {
-        };
-        try {
-            return GuiceContext.get(ObjectBinderKeys.DefaultObjectMapper).readValue(variableText, typeRef);
-        } catch (JsonProcessingException e) {
-            return new HashMap<>();
-        }
-
-    }
-    
-    /**
-     * @return
-     */
-    @SuppressWarnings("Convert2Diamond")
-    public List<Map<String, Object>> asListMap() {
-        String variableText = getVariableText();
-        TypeReference<List<Map<String, Object>>> typeRef
-                = new TypeReference<List<Map<String, Object>>>() {
-        };
-        try {
-            return GuiceContext.get(ObjectBinderKeys.DefaultObjectMapper).readValue(variableText, typeRef);
-        } catch (JsonProcessingException e) {
-            return new ArrayList<>();
-        }
-        
-    }
-    
-    
-    /**
-     * Returns the variable object in the format requested
-     *
-     * @param <T>       Generic type of JavaScript part
-     * @param classType
-     * @return
-     */
-    public <T> T as(Class<T> classType) {
-        try {
-            T instance = GuiceContext.get(classType);
-            return IJsonRepresentation.From(getVariableText(), classType);
-        } catch (Exception e) {
-            log.log(Level.WARNING, "Error in decoding Ajax Response Variable", e);
-            return null;
-        }
-    }
-
-    /**
-     * Gets a variable object or blank so it at least gets instantiated
-     *
-     * @return
-     */
-    public String getVariableText() {
-        return variableText;
-    }
-
-    /**
-     * Sets the variable name to a new object
-     *
-     * @param variableText
-     */
-    public void setVariableText(String variableText) {
-        this.variableText = variableText;
-    }
-
-    /**
-     * Returns the variable object in the format requested
-     *
-     * @param <T> Generic type of JavaScript part
-     * @return
-     */
-    public <T> T update(T object) {
-        try {
-            ObjectMapper om = GuiceContext.get(ObjectMapper.class);
-            om.readerForUpdating(object)
-                    .readValue(getVariableText());
-            return object;
-        } catch (Exception e) {
-            log.log(Level.WARNING, "Error in decoding Ajax Response Variable", e);
-            return null;
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        JsonVariable that = (JsonVariable) o;
-        return Objects.equals(getVariableName(), that.getVariableName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getVariableName());
-    }
+		extends JavaScriptPart<JsonVariable>
+{
+	private static final Logger log = LogFactory.getLog("JsonVariable");
+	
+	/**
+	 * The variable name to use
+	 */
+	private String variableName;
+	/**
+	 * The object to assign
+	 */
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@JsonDeserialize(using = ObjectToStringDeserialize.class)
+	@JsonSerialize(using = AlwaysEmptySerializer.class)
+	private String variableText;
+	/**
+	 * The actual variable object
+	 */
+	private Object variable;
+	/**
+	 * The owner id of this DTO
+	 */
+	@JsonProperty(value = "$jwid")
+	private String ownerId;
+	
+	/**
+	 * Constructs a blank object
+	 */
+	public JsonVariable()
+	{
+	}
+	
+	/**
+	 * Constructs a new instance with a given name and object
+	 *
+	 * @param variableName   The variable name to apply
+	 * @param variableObject The JSON object to use
+	 */
+	public JsonVariable(String variableName, String variableObject)
+	{
+		this.variableName = variableName;
+		variableText = variableObject;
+		variable = variableText;
+	}
+	
+	/**
+	 * Constructs a new instance with a given name and object
+	 *
+	 * @param variableName   The variable name to apply
+	 * @param variableObject The JSON object to use
+	 */
+	public JsonVariable(String variableName, Object variableObject)
+	{
+		this.variableName = variableName;
+		variable = variableObject;
+	}
+	
+	/**
+	 * Constructs a new instance with a given name and object
+	 *
+	 * @param variableName   The variable name to apply
+	 * @param variableObject The JSON object to use
+	 */
+	public JsonVariable(String variableName, JavaScriptPart<?> variableObject)
+	{
+		this.variableName = variableName;
+		variable = variableObject;
+	}
+	
+	/**
+	 * Gets the DTO Form of this object
+	 *
+	 * @param <T>       The type this object actually is
+	 * @param classType The class type to return
+	 * @return The DTO direct from the call
+	 * @throws IOException
+	 */
+	public <T extends Serializable> T getDto(Class<T> classType) throws IOException
+	{
+		T output = IJsonRepresentation.From(variableText, classType);
+		variable = output;
+		return output;
+	}
+	
+	/**
+	 * Gets the variable name
+	 *
+	 * @return
+	 */
+	public String getVariableName()
+	{
+		return variableName;
+	}
+	
+	/**
+	 * Sets the variable name
+	 *
+	 * @param variableName
+	 */
+	public void setVariableName(String variableName)
+	{
+		this.variableName = variableName;
+	}
+	
+	/**
+	 * Returns the variable if any is applied
+	 *
+	 * @return
+	 */
+	public Object getVariable()
+	{
+		return variable;
+	}
+	
+	/**
+	 * Sets the variable if any is applied
+	 *
+	 * @param variable
+	 */
+	public JsonVariable setVariable(Serializable variable)
+	{
+		this.variable = variable;
+		return this;
+	}
+	
+	/**
+	 * Sets the owning component ID
+	 *
+	 * @return
+	 */
+	@Override
+	public String getReferenceId()
+	{
+		return ownerId;
+	}
+	
+	/**
+	 * Gets the owning component ID
+	 *
+	 * @param referenceId
+	 */
+	@Override
+	public void setReferenceId(String referenceId)
+	{
+		ownerId = referenceId;
+	}
+	
+	/**
+	 * @return
+	 */
+	@SuppressWarnings("Convert2Diamond")
+	public Map<String, Object> asMap()
+	{
+		String variableText = getVariableText();
+		TypeReference<HashMap<String, Object>> typeRef
+				= new TypeReference<HashMap<String, Object>>()
+		{
+		};
+		try
+		{
+			return GuiceContext.get(ObjectBinderKeys.DefaultObjectMapper)
+			                   .readValue(variableText, typeRef);
+		}
+		catch (JsonProcessingException e)
+		{
+			return new HashMap<>();
+		}
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	@SuppressWarnings("Convert2Diamond")
+	public List<Map<String, Object>> asListMap()
+	{
+		String variableText = getVariableText();
+		TypeReference<List<Map<String, Object>>> typeRef
+				= new TypeReference<List<Map<String, Object>>>()
+		{
+		};
+		try
+		{
+			return GuiceContext.get(ObjectBinderKeys.DefaultObjectMapper)
+			                   .readValue(variableText, typeRef);
+		}
+		catch (JsonProcessingException e)
+		{
+			return new ArrayList<>();
+		}
+		
+	}
+	
+	
+	/**
+	 * Returns the variable object in the format requested
+	 *
+	 * @param <T>       Generic type of JavaScript part
+	 * @param classType
+	 * @return
+	 */
+	public <T> T as(Class<T> classType)
+	{
+		try
+		{
+			T instance = GuiceContext.get(classType);
+			return IJsonRepresentation.From(getVariableText(), classType);
+		}
+		catch (Exception e)
+		{
+			log.log(Level.WARNING, "Error in decoding Ajax Response Variable", e);
+			return null;
+		}
+	}
+	
+	/**
+	 * Gets a variable object or blank so it at least gets instantiated
+	 *
+	 * @return
+	 */
+	public String getVariableText()
+	{
+		return variableText;
+	}
+	
+	/**
+	 * Sets the variable name to a new object
+	 *
+	 * @param variableText
+	 */
+	public JsonVariable setVariableText(String variableText)
+	{
+		this.variableText = variableText;
+		return this;
+	}
+	
+	/**
+	 * Returns the variable object in the format requested
+	 *
+	 * @param <T> Generic type of JavaScript part
+	 * @return
+	 */
+	public <T> T update(T object)
+	{
+		try
+		{
+			ObjectMapper om = GuiceContext.get(ObjectMapper.class);
+			om.readerForUpdating(object)
+			  .readValue(getVariableText());
+			return object;
+		}
+		catch (Exception e)
+		{
+			log.log(Level.WARNING, "Error in decoding Ajax Response Variable", e);
+			return null;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (o == null || getClass() != o.getClass())
+		{
+			return false;
+		}
+		JsonVariable that = (JsonVariable) o;
+		return Objects.equals(getVariableName(), that.getVariableName());
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(getVariableName());
+	}
 }
